f073879c6fd74d199563dc4693b339d1285ff3a9
Wendy Hernández <wahaf13@hotmail.com>
2016-01-18 00:05:30 -0600

    Initial Commit

---
+++ tarea1_libreria/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  tarea1_libreria
+//
+//  Created by Wendy Hernandez on 17/01/16.
+//  Copyright © 2016 Wendy Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var resultado: UITextView!
+    @IBOutlet weak var busca_libro: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
